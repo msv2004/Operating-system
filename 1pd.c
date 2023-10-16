e5a651b7f61fcbd57f3576f26a2e7106b5ba8af0
Shashe Vikaash <128576150+msv2004@users.noreply.github.com>
2023-10-16 09:56:36 +0530

    Add files via upload

---
+++ 1pd.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+  pid_t child_pid = fork();
+
+  if (child_pid < 0) {
+    perror("fork() failed");
+    return 1;
+  }
+  pid_t current_pid = getpid();
+
+  pid_t parent_pid = getppid();
+
+  if (child_pid == 0) {
+    printf("Child process:\n");
+    printf("PID: %d\n", current_pid);
+    printf("Parent PID: %d\n", parent_pid);
+  } else {
+    printf("Parent process:\n");
+    printf("PID: %d\n", current_pid);
+    printf("Parent PID: %d\n", parent_pid);
+  }
+
+  return 0;
+}
+
